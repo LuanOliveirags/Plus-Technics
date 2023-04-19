ad22ed80cdf951a407d4f3f4c5d6cb05db137d74
Luan Oliveira <Luanoliveirags@gmail.com>
2023-04-19 09:17:35 -0300

    x

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ $(document).ready(function () {
         $(".menu").toggleClass("active");
 });
 });
+
+
+window.sr = ScrollReveal ({reset:true});
+sr.reveal ('.text-1',{rotate: {x:0, y:80, z:0 }});
+sr.reveal ('.text-2',{duration: 5000});
+
+sr.reveal ('p',{duration: 2000});
+
+
+sr.reveal ('.text-3',{rotate: {x:0, y:80, z:0 }});
+sr.reveal ('.text-4',{duration: 5000});
+
