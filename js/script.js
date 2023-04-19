8626780fe6cf199764c03eac7aa45ca397b97393
Luan Oliveira <Luanoliveirags@gmail.com>
2023-04-19 09:36:38 -0300

    x

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 
 
 window.sr = ScrollReveal ({reset:true});
-sr.reveal ('.text-1',{rotate: {x:0, y:80, z:0 }});
+sr.reveal ('.text-1',{rotate: {x:0, y:80, z:0}});
 sr.reveal ('.text-2',{duration: 5000});
 
 sr.reveal ('p',{duration: 2000});
