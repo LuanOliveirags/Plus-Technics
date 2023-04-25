7a36ac0327bb94161fe5c2fd99f602e954fd1553
Luan Oliveira <Luanoliveirags@gmail.com>
2023-04-25 15:41:53 -0300

    x

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 
 window.sr = ScrollReveal().reveal('.text-1', { delay: 1000 });
 window.sr = ScrollReveal().reveal('.card-image', { delay: 2000 });
-window.sr = ScrollReveal().reveal('.logo', {rotate: {x:80, y:10, z:0}},{duration: 6000});
+window.sr = ScrollReveal().reveal('.logo', {rotate: {x:0, y:900, z:0}});
 
 window.sr = ScrollReveal().reveal('.menu', { delay: 1500 });
 window.sr = ScrollReveal().reveal('.hamburguer', { delay: 1400 });
@@ -17,7 +17,7 @@ window.sr = ScrollReveal().reveal('.hamburguer', { delay: 1400 });
 
 
 window.sr = ScrollReveal ({reset:true});
-sr.reveal ('.text-1',{rotate: {x:0, y:80, z:0}});
+sr.reveal ('.text-1',{rotate: {x:0, y:80, z:0}},{duration: 3000});
 sr.reveal ('.text-2',{duration: 3000});
 
 sr.reveal ('p',{duration: 2000});
