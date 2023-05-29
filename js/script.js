a40565ec0ea564eeb8e35fff40bc5c9ef4e5dde6
Luan Oliveira <Luanoliveirags@gmail.com>
2023-05-29 11:43:44 -0300

    xx

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ sr.reveal ('.text-2',{duration: 3000});
 
 sr.reveal ('p',{duration: 2000});
 sr.reveal ('.text-3',{rotate: {x:0, y:80, z:0 }});
+sr.reveal ('.logo',{rotate: {x:0, y:80, z:0 }});
 sr.reveal ('.text-4',{duration: 5000});
 
 
